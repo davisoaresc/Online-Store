6395abb50080a593c0497e95aa93e17d8319e7a5
davisoaresc <davisoarescoura@gmail.com>
2022-05-08 16:46:33 +0200

    Feat: sequelize config

---
+++ backend/database/config/config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
+    database: 'online-store',
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    define: {
+      timestamps: false,
+    },
+  }
+};
