c58617801b9de631c88d0f228552d3dcbe59e9fb
davisoaresc <davisoarescoura@gmail.com>
2022-05-12 12:13:05 +0200

    Feat: dropdown to list user details

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,22 +1,58 @@
-import { useContext } from 'react';
-import { BiShoppingBag } from 'react-icons/bi';
-import { CgProfile } from 'react-icons/cg';
-import Context from '../Context/Context';
+import { useEffect, useState } from 'react';
+import { SignOut, ShoppingBagOpen, DesktopTower } from "phosphor-react";
+import { Link } from 'react-router-dom';
 import '../Styles/Header.css';
-import { ModalAddProduct } from './ModalAddProduct';
+import { WidgetUserDetails } from './WidgetUserDetails';
+import { DropDownListProdusct } from './DropdownListProduct';
+import { getUserByUsername } from '../Helpers/fetchApi';
 
 export function Header() {
-  const { user } = useContext(Context);
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [pontos, setPontos] = useState('');
+  const [moedas, setMoedas] = useState('');
+
+  function handleClick() {
+    localStorage.clear();
+  }
+
+  async function getItemLocalStorage() {
+    const localStorageItem = await JSON.parse(localStorage.getItem('user'));
+    setUser(localStorageItem.userName);
+    const data = await getUserByUsername(localStorageItem.token, localStorageItem.userName);
+    setEmail(data.email)
+    setPontos(data.pontos)
+    setMoedas(data.moedas)
+  }
+
+  useEffect(() => {
+    getItemLocalStorage();
+  }, []);
+
   return (
     <header className="header">
       <div className="wallet-infos-header">
-        <span><BiShoppingBag size={45} className="react-icons"/> </span>
+        <span><DesktopTower size={55} /> </span>
       </div>
+
       <div className="user-container">
-        { user === "admin"  && <ModalAddProduct /> }
-        <span><CgProfile size={ 40 } className="react-icons"/></span>
-        <p className="user">{user}</p>
+
+        { user === "admin"  && <DropDownListProdusct/> }
+
+        <WidgetUserDetails
+          user={ user }
+          email={email}
+          pontos={pontos}
+          moedas={moedas}       
+        />
+        <Link 
+          to={"/login"}
+          onClick={handleClick}
+        >
+          <SignOut size={35} color="white"/>
+        </Link>
       </div>
+
     </header>
   )
 }
