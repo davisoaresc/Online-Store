31d8f21e6898901ea977617c67e29d5c70b89512
davisoaresc <davisoarescoura@gmail.com>
2022-05-14 20:51:48 +0200

    Feat: end point to update user coins

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,21 +1,25 @@
-const { User } =  require('../database/models');
+const { User } =  require('../models');
 
 const createUser = async (req, res, next) => {
   try {
     const { usuario, email, senha } = req.body;
+    const pontos = 0;
+    const moedas = 0;
 
-    const findEmail = await User.findOne({ where: { email } });
+    const findUser = await User.findOne({ where: { email } });
 
-    if (findEmail)
+    if (findUser) {
       return res.status(409).json({ message: "Usuário já registrado" });
-
+    }
     const userCreated = await User.create({
       usuario,
       email,
       senha,
+      pontos,
+      moedas,
     });
 
-    return res.status(201).json({ userCreated });
+    return res.status(201).json({message: "Usuario criado com sucesso"});
   } catch (err) {
     next(err);
   }
@@ -24,7 +28,6 @@ const createUser = async (req, res, next) => {
 const listUsersByUserName = async (req, res, next) => {
   try {
     const { usuario } = req.params;
-    console.log(usuario);
     const findUser = await User.findOne({ where: { usuario } });
     if(!findUser) {
       return res.status(404).json({message: "Usuário não encontrado"})
@@ -35,8 +38,40 @@ const listUsersByUserName = async (req, res, next) => {
   }
 };
 
+const updateUserCoins = async (req, res, next) => {
+  try {
+    const { usuario } = req.params;
+    const findUser = await User.findOne({ where: { usuario } });
+    console.log(findUser);
+
+    if (findUser.moedas < req.body.moedas ) {
+      return res.status(401).json({message: "Saldo de moedas insuficiente"})
+    }
+    await User.increment({ pontos:req.body.pontos },{ where: { usuario } });
+    await User.decrement({ moedas:req.body.moedas },{ where: { usuario } });
+    await User.increment({ moedas:req.body.moedas },{ where: { usuario: "admin" } });
+
+    return res.status(200).json({message: "Compra efetuada com sucesso"});
+
+  } catch (error) {
+    return next(error);
+  }
+}
+
+const userBuyingCoins = async (req, res, next) => {
+  try {
+    const { usuario } = req.params
+    await User.increment({ moedas:req.body.moedas },{ where: { usuario } });
+    return res.status(200).json({message:"Moedas compradas com sucesso!"});
+  } catch (error) {
+    return next(error);
+  }
+}
+
 module.exports = {
   createUser,
   listUsersByUserName,
+  updateUserCoins,
+  userBuyingCoins,
 };
 
--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,10 +1,12 @@
-const { createUser, listUsersByUserName } = require('./user');
+const { createUser, listUsersByUserName, updateUserCoins, userBuyingCoins } = require('./user');
 const { createProduct, listProducts } = require('./product');
 const login  = require('./login');
 
 module.exports = {
   createUser,
   listUsersByUserName,
+  updateUserCoins,
+  userBuyingCoins,
   createProduct,
   listProducts,
   login,
